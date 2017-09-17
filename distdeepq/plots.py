51d9209d472fa50f0ee9cdafb821eb701fc68f02
Silvestr Stanko <stanksil@fel.cvut.cz>
2017-09-17 20:56:23 +0200

    speedup

--- distdeepq/plots.py
+++ distdeepq/plots.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 from distdeepq.static import build_z
 import tensorflow as tf
 
+
 class PlotMachine:
 
     def __init__(self, dist_params, nb_actions):
 
         self.z, self.dz = build_z(**dist_params, numpy=True)
 
-        h_plots = int(np.sqrt(nb_actions))
-        v_plots = int(np.ceil(nb_actions / h_plots))
-
         plt.ion()
-        # self.fig, self.ax = plt.subplots(v_plots, h_plots, sharex=True, sharey=True)
+
         self.fig, self.ax = plt.subplots()
+
+        self.bars = [self.ax.bar(self.z, np.ones_like(self.z)*0.25, self.dz * 0.9) for _ in range(nb_actions)]
 
         self.sess = tf.get_default_session()
         self.p_out = tf.get_default_graph().get_tensor_by_name("distdeepq/q_func/cnn_softmax:0")
@@ -25,15 +25,10 @@ class PlotMachine:
     def plot_distribution(self, obs):
         pdf_act = self.make_pdf(obs)
 
-        # multiple plots
-        # for sample, ax in zip(pdf_act, self.ax.flatten()):
-        #     ax.clear()
-        #     ax.bar(self.z, sample, self.dz)
+        for rects, sample in zip(self.bars, pdf_act):
+            for rect, y in zip(rects, sample):
+                rect.set_height(y)
 
-        # single plot
-        self.ax.clear()
-        for sample in pdf_act:
-            self.ax.bar(self.z, sample, self.dz * 0.9)
-
-        plt.pause(0.001)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
 
